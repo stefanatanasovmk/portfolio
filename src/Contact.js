97ef29bb90d5d4b76a7264a5d06c2f56d8ac9d11
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-06-01 16:03:01 +0300

    improved responsivnes & design

--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Typer from "./Typer";
 import "./Contact.css"
-const text = "If you want to contact me, write me on my email:"
-const email = "atanasovstefan@hotmail.com"
+const text = `If you want to contact me, write me on my email: atanasovstefan@hotmail.com`
+
 export default function Contact() {
      return (
           <div id="contact" className="Contact">
                <Typer text={text} />
-               <a href="mailto:atanasovstefan@hotmail.com"><Typer text={email} /></a>
-
           </div>
      )
 }
--- src/Work.js
+++ src/Work.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Work.css"
 import Typer from "./Typer";
 import invoiceMkImage from "./images/invoice.mk.png"
+import sisustuspisteImage from "./images/sisustuspistePisteCalculator.png"
 const text = "My work so far..."
 export default function Work() {
      return (
@@ -12,6 +13,11 @@ export default function Work() {
                          <a href="https://www.invoice.mk/" target="_blank" rel="noreferrer"><img src={invoiceMkImage} alt="Invoice.mk project" className="Image" /></a>
                          <p className="ProjectText"><b>Invoice.mk</b>
                               - free to use service for generating invoices for small businesses in Macedonia.</p>
+                    </div>
+                    <div className="Project">
+                         <a href="https://dynamic-kelpie-fd8f04.netlify.app/" target="_blank" rel="noreferrer"><img src={sisustuspisteImage} alt="Sisustuspiste Calculator Project" className="Image" /></a>
+                         <p className="ProjectText"><b>Grout Calculator</b>
+                              - calculator for grout, made for a local hardware store.</p>
                     </div>
                </div>
           </div>
