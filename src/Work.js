d83ddede178d089e12caaf510bba53b294c22c13
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-08-01 12:05:01 +0300

    added cyclehelsinki project in Work.js

--- src/Work.js
+++ src/Work.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Work.css"
 import Typer from "./Typer";
 import invoiceMkImage from "./images/invoice.mk.png"
-// import sisustuspisteImage from "./images/sisustuspistePisteCalculator.png"
+import cycleHelsinki from "./images/cyclehelsinki.fi.png"
 const text = "My work so far..."
 export default function Work() {
      return (
@@ -14,11 +14,11 @@ export default function Work() {
                          <p className="ProjectText"><b>Invoice.mk</b>
                               - free to use service for generating invoices for small businesses in Macedonia.</p>
                     </div>
-                    {/* <div className="Project">
-                         <a href="https://dynamic-kelpie-fd8f04.netlify.app/" target="_blank" rel="noreferrer"><img src={sisustuspisteImage} alt="Sisustuspiste Calculator Project" className="Image" /></a>
-                         <p className="ProjectText"><b>Grout Calculator</b>
-                              - calculator for grout, made for a local hardware store.</p>
-                    </div> */}
+                    <div className="Project">
+                         <a href="https://cyclehelsinki.fi/" target="_blank" rel="noreferrer"><img src={cycleHelsinki} alt="Cycle Helsinki Project" className="Image" /></a>
+                         <p className="ProjectText"><b>CycleHelsinki.fi</b>
+                              - app for bicycle sharing stations in Helsinki area.</p>
+                    </div>
                </div>
           </div>
 
